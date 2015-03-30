6e79f2348334c3fb42009f29e4ae772f810ec9c6
jfhuang <korvin101@gmail.com>
2015-03-30 13:12:18 +0800

    各小demo整合成一个

---
+++ app2/src/main/java/com/example/korvi_000/myapplication/DotActivity.java
@@ -0,0 +1,16 @@
+package com.example.korvi_000.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Tom on 2015/3/12.
+ */
+public class DotActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dot_main);
+    }
+}
